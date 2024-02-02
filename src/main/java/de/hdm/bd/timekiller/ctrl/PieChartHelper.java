2ee624c277f108caff2b4cfb438d1b4751d98cf1
Carolin Calvetti <cc033@hdm-stuttgart.de>
2024-02-02 14:02:08 +0100

    bugfix gui

--- src/main/java/de/hdm/bd/timekiller/ctrl/PieChartHelper.java
+++ src/main/java/de/hdm/bd/timekiller/ctrl/PieChartHelper.java
@@ -47,7 +47,6 @@ public class PieChartHelper {
                 System.out.println("active Task: "+ task);
                 pieChartData.add(new PieChart.Data(task.getName(), task.getOverallDuration()));
                 System.out.println("Overallduration from task: "+ task.getName()+ " ist: "+ task.getOverallDuration());
-                task.clearDuration();
             }else if (task.getOverallDuration() != 0){
                 System.out.println("zuvor aktiver Task: "+ task);
                 pieChartData.add(new PieChart.Data(task.getName(), task.getOverallDuration()));
